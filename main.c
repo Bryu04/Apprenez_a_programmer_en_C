7bda15fca90a055cd0e9d6ba9522590636158be6
Jean-Paul Melisse <melisse.jeanpaulpro04@gmail.com>
2025-04-08 15:39:13 +0200

    Création et modification de mon premier programme en C

---
+++ main.c
@@ -0,0 +1,18 @@
+//
+// main.c
+// monpremierprogramme
+//
+// Created by Jean-Paul MELISSE on 08/04/2025
+//
+
+ // les 2 libraries suivantes pour afficher du texte à l'écran
+#include <stdio.h>   
+#include <stdlib.h>
+
+// Fonction principale : main
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");  // afficher le message
+    printf("Bonjour :)\n");
+    return 0;  // retourne le code de sortie 0
+}
