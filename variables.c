50293810c9535dda440d8bd6cfb0cfb058a0e150
Jean-Paul Melisse <melisse.jeanpaulpro04@gmail.com>
2025-04-09 09:45:36 +0200

    Manipuler les variables et les constantes en C

--- variables.c
+++ variables.c
@@ -3,6 +3,16 @@
 
 int main(int argc, const char * argv[]) {
     int nombre_de_vies;
-
+    nombre_de_vies = 5;
+    printf("Nombre : %d\n",nombre_de_vies);
+    nombre_de_vies = 4;
+    printf("Je suis touchE. Nombre restant : %d\n",nombre_de_vies);
+    const int Attack = 10;
+    printf("Point d'attaque : %d\n",Attack);
+    // J'essaie augmenter mes point d'attaque de base
+    //Attack = 20;
+    //printf("Point d'attaque augmentE : %d\n",Attack);
+    //ça ne marche pas parce que la variable Attack est une 
+    //constante
     return 0;
 }
