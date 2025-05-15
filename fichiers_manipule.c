f385638591239531c54ac61c51a329f0b434f8ac
Jean-Paul Melisse <melisse.jeanpaulpro04@gmail.com>
2025-05-15 12:39:40 +0200

    Ecrit dans des fichiers en utilisant les fonctions fputc, fputs et fprintf

--- fichiers_manipule.c
+++ fichiers_manipule.c
@@ -24,6 +24,7 @@ int main(int argc, const char * argv[]) {
         // et on ferme notre fichier quand on a fini
         printf("et on ferme notre fichier quand on a fini\n");
         fclose(monfichier);
+        printf("-----------------------------------\n");
     }
     else
     {
@@ -33,8 +34,64 @@ int main(int argc, const char * argv[]) {
         printf("et on ferme le fichier après création par l'ouverture\n");
         fclose(monfichier);
         printf("Ficher créé");
+        printf("-----------------------------------\n");
     }
 
+    // Maintenant qu'on a vu comment ouvrir un fichier, on va l'éditeur
+    // On réinitie la variable monfichier avant d'ouvrir un autre fichier
+    monfichier = NULL;
+    monfichier = fopen("test_write.txt", "w");
+    // Utilisation de la fonction fputc
+    // La fonction fputc permet d'écrire un caractère dans un fichier.
+    if (monfichier != NULL)
+    {
+        printf("---- Ecriture d'un caractere ------\n");
+        fputc('C', monfichier);
+        fclose(monfichier);
+        printf("Fin de l'écriture\n");
+        printf("-----------------------------------\n");
+    }
+
+
+    // Utilisation de la fonction fputs
+    // La fonction fputs, comparé à fputc, permet d'écrire une chaine de caractères dans
+    // un fichier.
+    // On réinitie la variable monfichier avant d'ouvrir un autre fichier
+    monfichier = NULL;
+    monfichier = fopen("test_write_2.txt", "w");
+
+    if (monfichier != NULL)
+    {
+        printf("---- Ecriture d'une chaine de caracteres ------\n");
+        fputs("Hello guys !!\nWelcome to my site.", monfichier);
+        fclose(monfichier);
+        printf("Fin de l'écriture\n");
+        printf("-----------------------------------------------\n");
+    }
+
+    // Utilisation de la fonction fprintf
+    // La fonction fprintf ressemble beaucoup à printf sauf elle écrit le contenu dans un
+    // fichier. (Un peu comme sprintf qui écrit le contenu dans une chaine de caractères)
+    int age = 0;
+    char nom[100];
+    // On réinitie la variable monfichier avant d'ouvrir un autre fichier
+    monfichier = NULL;
+    monfichier = fopen("test_write_3.txt", "w");
+
+    if (monfichier != NULL)
+    {
+        printf("Quel est votre nom ? : ");
+        scanf("%s", nom);
+        printf("Quel est votre age ? : ");
+        scanf("%d", &age);
+        
+        printf("---- Ecriture des informations dans un fichier ------\n");
+        fprintf(monfichier, "Monsieur %s, qui utilise le programme, dit qu'il a %d ans",
+                nom, age);
+        fclose(monfichier);
+        printf("Fin de l'écriture\n");
+        printf("-----------------------------------------------------\n");
+    }
 
     return 0;
 }
